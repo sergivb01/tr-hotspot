9cdedd960a3f54ba10ce0986d8fcbfd9f1cf669b
sergivb01 <sergivb01@gmail.com>
2019-01-07 19:03:34 +0100

    .

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -17,9 +17,7 @@ router.get('/google', passport.authenticate('google', {
     'email',
     'profile',
     'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/plus.me',
-    'https://www.googleapis.com/auth/plus.login'
+    'https://www.googleapis.com/auth/userinfo.profile'
   ]
 }))
 
@@ -30,9 +28,7 @@ router.get('/google/redirect', passport.authenticate('google', {
     'email',
     'profile',
     'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/plus.me',
-    'https://www.googleapis.com/auth/plus.login'
+    'https://www.googleapis.com/auth/userinfo.profile'
   ]
 }), (req, res) => {
   let details = req.session.details
